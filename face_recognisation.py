2d70dc188f7d0f6699098340bc1a708751c569b5
manik2158 <45430505+manik2158@users.noreply.github.com>
2021-08-02 12:04:29 +0530

    Update face_recognisation.py

--- face_recognisation.py
+++ face_recognisation.py
@@ -50,7 +50,7 @@ class smallmodel:
 
 
 
-        model.compile(loss='sparse_categorical_crossentropy',
+        model.compile(loss='categorical_crossentropy',
                     optimizer='rmsprop',
                     metrics=['accuracy'])
 
@@ -58,7 +58,7 @@ class smallmodel:
         train_datagen = ImageDataGenerator(
             rescale=1. / 255,
             shear_range=0.2,
-            zoom_range=0.2,
+            zoom_range=0.15,
             horizontal_flip=True)
 
         # this is the augmentation configuration we will use for testing:
